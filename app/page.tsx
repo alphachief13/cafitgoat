ee66cbaeeb5e52efec93bbab067258442ce7940c
Victor <victormvcordeiro@gmail.com>
2025-08-25 16:20:03 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,122 @@
-import Image from "next/image";
+"use client";
+
+import Lenis from "lenis";
+import { useEffect } from "react";
+import Balatro from "@/components/Balatro";
+import TiltedCard from "@/components/TiltedCard";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { accordionContentData } from "@/data/main-content";
+import BlurText from "@/components/BlurText";
+
+function IntroSection() {
+  return (
+    <div className="w-screen h-screen relative">
+      <div className="-z-10 absolute inset-0">
+        <Balatro
+          color1="#162325"
+          color2="#000000"
+          isRotate={false}
+          mouseInteraction={true}
+          pixelFilter={700}
+        />
+      </div>
+
+      <div className="w-full h-full flex items-center justify-center flex-col gap-10">
+        <BlurText
+          text="João Lucas da Rosa, editor"
+          delay={250}
+          animateBy="words"
+          direction="top"
+          className="font-bold text-5xl text-white text-centerF"
+        />
+        <TiltedCard
+          imageSrc="pfp-1.jpg"
+          altText=""
+          captionText="João Lucas, 18"
+          containerHeight="300px"
+          containerWidth="300px"
+          imageHeight="300px"
+          imageWidth="300px"
+          rotateAmplitude={12}
+          scaleOnHover={1.2}
+          showMobileWarning={false}
+          showTooltip={true}
+          displayOverlayContent={true}
+          overlayContent={<></>}
+        />
+      </div>
+
+      {/* <CircularText
+        text="*CAFITGOAT*CAFITGOAT"
+        onHover="speedUp"
+        spinDuration={20}
+        className="custom-class"
+      /> */}
+    </div>
+  );
+}
+
+function AboutMeAccordion() {
+  return (
+    <div className="w-full">
+      <Accordion type="single" collapsible className="w-full">
+        {accordionContentData.map((item, index) => (
+          <AccordionItem
+            className="bg-[#162325] text-white p-5"
+            key={index}
+            value={`item-${index}`}
+          >
+            <AccordionTrigger className="text-3xl font-bold">
+              {item.title}
+            </AccordionTrigger>
+            <AccordionContent className="text-base">
+              <div dangerouslySetInnerHTML={{ __html: item.description }} />
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    requestAnimationFrame(raf);
+  }, []);
+
+  const handleAnimationComplete = () => {
+    // console.log("Animation completed!");
+  };
+
+  return (
+    <div className="overflow-hidden">
+      <IntroSection />
+
+      <div className="bg-[#162325] py-10 flex flex-col justify-center items-center">
+        <BlurText
+          text="Commentary Videos"
+          delay={400}
+          animateBy="words"
+          direction="top"
+          onAnimationComplete={handleAnimationComplete}
+          className="text-5xl font-bold text-white"
+        />
+
+        
+      </div>
+
+      <AboutMeAccordion />
     </div>
   );
 }
---
+++ data/main-content.ts
@@ -0,0 +1,44 @@
+
+interface AccordionData {
+  title: string;
+  description: string;
+}
+
+export const accordionContentData: AccordionData[] = [
+  {
+    title: "About Me",
+    description: `
+      My name is <strong>João Lucas da Rosa</strong>, I’m 18 years old and live in the southern region of Brazil.<br />
+      Native language: Portuguese <br />
+      <strong>Professional English</strong> <br />
+      Main focus: <strong>Video Editing</strong><br />
+      Hobby: <strong>Gaming</strong><br />
+      Always seeking improvement and excellence.
+    `,
+  },
+  {
+    title: "Software",
+    description: `
+      I use <strong>DaVinci Resolve</strong> as my main editing tool: <br />
+      – Video editing <br />
+      – Color correction <br />
+      – Visual effects <br />
+      – Audio post-production
+    `,
+  },
+  {
+    title: "Availability",
+    description: `
+      <strong>Tue, Thu, Sat, Sun</strong> → 2pm – 12am (BRT, UTC-3) <br />
+      <strong>Mon, Wed, Fri</strong> → 6pm – 12am (BRT, UTC-3)
+    `,
+  },
+  {
+    title: "Contact",
+    description: `
+      <strong>Discord:</strong> cafitgoat <br />
+      <strong>Reddit:</strong> u/MeiaaDoiss <br />
+      <strong>Twitter (X):</strong> @cafitgoat
+    `,
+  },
+];
